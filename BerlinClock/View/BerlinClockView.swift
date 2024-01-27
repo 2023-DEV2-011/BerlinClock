a70d55527ffc4fa5300a77ae36f6266dd38db77e
2023-DEV2-011 <>
2024-01-27 18:54:33 +0100

    Add empty BerlinClockView

---
+++ BerlinClock/View/BerlinClockView.swift
@@ -0,0 +1,18 @@
+//
+//  BerlinClockView.swift
+//  BerlinClock
+//
+//  Created on 27/01/2024.
+//
+
+import SwiftUI
+
+struct BerlinClockView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    BerlinClockView()
+}
